42d0629b58e1c18e77bb635ccf73172228c2b3b9
Kapil Chawla <kc3020@gmail.com>
2022-04-03 23:07:57 +0100

    changed

--- main.tf
+++ main.tf
@@ -11,11 +11,11 @@ provider "docker" {
   host = "unix:///var/run/docker.sock"
 }
 
-resource "docker_image" "test_image" {
+resource "docker_image" "nodered_image" {
   name = "nodered/node-red"
 }
 
-resource "docker_container" "doc_con" {
+resource "docker_container" "nodered_container" {
   name  = "nodered"
   image = docker_image.test_image.latest
 
